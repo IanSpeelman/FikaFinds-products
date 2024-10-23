70d99fd79b37ebe90e08b54134e07810d28effb2
Ian Speelman <ianspeelman@gmail.com>
2024-10-23 18:36:33 +0200

    fix style issue

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,8 +2,8 @@ export type Product = {
     id?: number,
     name: string,
     image: string,
-    price: number
-    category: string
-    stock?: number
+    price: number,
+    category: string,
+    stock?: number,
     amount?: number
 }
